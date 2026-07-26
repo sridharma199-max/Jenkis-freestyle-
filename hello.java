2ba15159ae6c939d93688fa16440c41ce17368bf
sridharma199-max <sridharma199@gmail.com>
2026-07-26 21:06:18 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Bring it for Sri Dharma");
         System.out.println("All hail");
+        System.out.println("Bring it for Sri Dharma");
+        System.out.println("All hail");
+
     }
 }
